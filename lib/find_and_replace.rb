599468b8105ee0382be1868dc10f6bd6a2341f57
Adam McFadden & Toby Alden <student@epicodus.com>
2015-04-22 16:54:55 -0700

    Finish progam. Works as requested.

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,15 +1,5 @@
 class String
   define_method(:find_and_replace) do |find, replace|
-
-  self.gsub(find, replace)
-    # split_string = self.split(" ")
-    # split_string.each() do |word|
-    #   if(word.eql?(find))
-    #     word.replace(replace)
-    #   end
-    # end
-
-    #split_string.join(" ")
-
+    self.gsub(find, replace)
   end
 end
